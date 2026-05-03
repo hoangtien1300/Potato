9887e02bc652f40979592a7e3247e70e75162b00
hoangtien1300 <hoangtien1300@users.noreply.github.com>
2026-05-03 13:48:24 +0700

    Build OperationView and update mapping

--- scratch/check_headers.js
+++ scratch/check_headers.js
@@ -1,21 +1,6 @@
 
-const spreadsheetId = '1dTcxPgSS2olUtgjjk2ZUvUo8e53Vi6J5Kk4bynKL0OE';
-const gid = '1019913137';
-const url = `https://docs.google.com/spreadsheets/d/${spreadsheetId}/export?format=csv&gid=${gid}`;
+const fs = require('fs');
+const content = fs.readFileSync('index.html', 'utf8');
 
-async function checkHeaders() {
-    try {
-        const response = await fetch(url);
-        const csvText = await response.text();
-        const headers = csvText.split('\n')[0];
-        console.log("Headers:", headers);
-        
-        // Also show first row of data to see if it matches
-        const firstRow = csvText.split('\n')[1];
-        console.log("First row:", firstRow);
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
-
-checkHeaders();
+// The Dashboard sheet gid is usually 0 or something I found earlier.
+// Wait, I can just check the mapClassDashboard function again to see what it uses.
